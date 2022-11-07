eaae8da3ef8463df09b6128308d25b1c839d9441
Ana-Ingrid <ingridsantoscosta2003@gmail.com>
2022-11-07 19:47:57 -0300

    Primeiro Commit
    O projeto foi desenvolvido até essa versão sem utilizar git ou github.

---
+++ index.js
@@ -0,0 +1,6 @@
+const botaomenu = document.querySelector('.cabecalho__menu');
+const menu = document.querySelector('.menu__lateral');
+
+botaomenu.addEventListener('click', () => {
+        menu.classList.toggle('menu__lateral__ativo')
+} )
